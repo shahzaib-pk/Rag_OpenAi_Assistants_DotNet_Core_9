a5bcd8eea1c1ac1c72f0946dba467b3dc8672bd8
Shah Zeb <shahzaibrind6@gmail.com>
2025-04-24 22:39:08 +0500

    Add project files.

---
+++ OpenAiRag/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
